1dd6a64dbce77fa3eea5ba7b76c3906f2af38342
rpuhalovich <reanpuhalovich@gmail.com>
2021-05-17 23:21:04 +1000

    Added forward compatability to macos opengl?

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,12 @@ int main(void) {
     /* Initialize the library */
     if (!glfwInit()) return -1;
 
+    // stuff to make sure that macos uses the latest version of OpenGL?
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(1024, 768, "Ye Haw", NULL, NULL);
     if (!window) {
