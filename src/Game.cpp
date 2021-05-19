d0e263a9882e6705cd10ca1c440d0b5eff173d24
Ryan P <reanpuhalovich@gmail.com>
2021-05-19 19:46:54 +1000

    Created dark green screen with glfw.

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,8 +6,77 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+static void glfwWindowSetup(bool maximized) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    if (maximized) {
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+    }
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+}
+
+static GLFWwindow* makeWindow(int width, int height, bool maximized) {
+    glfwWindowSetup(maximized);
+    // GLFWmonitor* m = glfwGetPrimaryMonitor();
+    // GLFWwindow* window = glfwCreateWindow(800, 600, "Ryan's first window.", m, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Ryan's first window.", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create window\n";
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    return window;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void gladInit(GLFWwindow* window, int width, int height) {
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    glViewport(0, 0, width, height);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+struct winInfo {
+    int width;
+    int height;
+};
+
 int main(void) {
-    std::cout << "Hey world" << std::endl;
-    std::cin.get();
+    winInfo wi = { 800, 600 };
+    GLFWwindow* window = makeWindow(wi.width, wi.height, false);
+    gladInit(window, wi.width, wi.height);
+
+    while (!glfwWindowShouldClose(window)) {
+        // check input
+        processInput(window);
+
+        //render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // check and call events and swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
