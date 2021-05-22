fb153a2c55185fa77751fc6e7c155103ef4e26b1
Ryan P <reanpuhalovich@gmail.com>
2021-05-22 22:43:17 +1000

    Added notes on how tf the process of drawing an object to the screen works in opengl.

--- src/Game.cpp
+++ src/Game.cpp
@@ -24,14 +24,26 @@ int main(void) {
     std::unique_ptr<Window> w (new Window(800, 600, "Mah Window", false));
     std::unique_ptr<ShaderProgram> sp (new ShaderProgram(vertexShaderSource, fragmentShaderSource));
 
+    /**
+     * Process for creating an object to draw to the screen.
+     * 1. get vertecies and indicies
+     * 2. generate the vao that you're going to fill with vbos and ebos and bind it
+     * 3. bind vbo and fill with verts, do the same with ebo and indicies
+     * 4. set up the glvertexattribpointer settings and enable it
+     * 5. unbind the latest GL_ARRAY_BUFFER (be it a vbo or ebo) and unbind the vao
+     * 
+     * The vao is a container for the vbo and ebo objects that you're to use to draw to the screen.
+     * You then rebind the vao that you want to draw to the screen and then draw it.
+     * Ensure you're using glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0) to draw using the ebo.
+     */
+
     float verts[] = {
          0.5f,  0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
         -0.5f, -0.5f, 0.0f,
         -0.5f,  0.5f, 0.0f
     };
-    
-    // don't put these as floats, unsigned ints only!!
+
     unsigned int indices[] = {
         0, 1, 3,
         1, 2, 3
@@ -41,7 +53,7 @@ int main(void) {
     glc(glGenVertexArrays(1, &vao));
     glc(glGenBuffers(1, &vbo));
     glc(glGenBuffers(1, &ebo));
-   
+
     // 1. bind Vertex Array Object
     glc(glBindVertexArray(vao));
 
@@ -56,9 +68,9 @@ int main(void) {
     // 4. then set the vertex attributes pointers
     glc(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0));
     glc(glEnableVertexAttribArray(0));
-    
+
     glc(glBindBuffer(GL_ARRAY_BUFFER, 0));
-    glc(glBindVertexArray(0));
+    glc(glBindVertexArray(0)); // unbinds vao for later use
 
     while (!w->shouldWindowClose()) {
         // check input
