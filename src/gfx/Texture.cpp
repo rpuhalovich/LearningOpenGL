57d2c5960b02b38da4f5717392a04f5cfc718f1e
rpuhalovich <reanpuhalovich@gmail.com>
2021-06-02 18:45:42 +1000

    Found out why my texloc function isn't working. For another day.

--- src/gfx/Texture.cpp
+++ src/gfx/Texture.cpp
@@ -9,5 +9,6 @@ Texture::~Texture()
 }
 
 const char* Texture::texloc(const char* texname) {
+    // TODO: malloc this, you're returning the address of a local scope object...
     return (std::string(RES_DIR) + std::string(texname)).c_str();
 }
