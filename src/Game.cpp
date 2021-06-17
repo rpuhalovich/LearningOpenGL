fa1b0a96e2b3ea77c08990e5573d5db43d1fe083
rpuhalovich <reanpuhalovich@gmail.com>
2021-06-17 15:27:32 +1000

    Now the crate rotates.

--- src/Game.cpp
+++ src/Game.cpp
@@ -27,18 +27,18 @@ int main(void) {
 
     sp1->useProgram();
 
-    // container transformation
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-
-    glc(unsigned int transformLoc = glGetUniformLocation(sp1->getShaderProgram(), "transform"));
-    glc(glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans)));
-
-
     while (!w->shouldWindowClose()) {
         // check input
         w->processInput();
+
+        // container transformation
+        glm::mat4 trans = glm::mat4(1.0f);
+        trans = glm::translate(trans, glm::vec3(-0.5, 0.5, 0.0));
+        trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
+
+        glc(unsigned int transformLoc = glGetUniformLocation(sp1->getShaderProgram(), "transform"));
+        glc(glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans)));
 
         // render clear colour (bg)
         glc(glClearColor(0.1f, 0.1f, 0.1f, 1.0f));
